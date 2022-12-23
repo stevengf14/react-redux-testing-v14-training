67e0fc69ed74ee034fee57be5881c65bf0086dde
Steven <stevengf14@gmail.com>
2022-12-22 23:39:08 -0500

    Comment Box Tests

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -1,7 +1,11 @@
 import { renderComponent, expect } from "../test_helper";
+import CommentBox from "../../src/components/comment_box";
 
 describe("CommentBox", () => {
-  it("has a text area", () => {});
+  it("has a text area", () => {
+    const component = renderComponent(CommentBox);
+    expect(component.find("textarea")).to.exist;
+  });
 
   it("has a button", () => {});
 });
