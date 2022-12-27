3504afc53789f9ebda2e5ac30cc9ecd359181e96
Steven <stevengf14@gmail.com>
2022-12-26 21:07:25 -0500

    beforeEach

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -2,8 +2,13 @@ import { renderComponent, expect } from "../test_helper";
 import CommentBox from "../../src/components/comment_box";
 
 describe("CommentBox", () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it("has the correct class", () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class("comment-box");
   });
 
