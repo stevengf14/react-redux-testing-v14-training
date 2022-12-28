c02a91daad124519b09b0ccf0cc99f772164a0b8
Steven <stevengf14@gmail.com>
2022-12-27 20:54:21 -0500

    Expecting Child Elements

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,18 +1,14 @@
 import { renderComponent, expect } from "../test_helper";
 import App from "../../src/components/app";
 
-// Use 'describe' to group together similar tests
 describe("App", () => {
   let component;
 
   beforeEach(() => {
-    // create an instance of App
     component = renderComponent(App);
   });
 
-  // Use 'it' to test a single attribute of a target
-  it("shows the correct text", () => {
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain("React simple starter");
+  it("shows a comment box", () => {
+    expect(component.find(".comment-box")).to.exist;
   });
 });
