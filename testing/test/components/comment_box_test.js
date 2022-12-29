c3565c5aaede7835044d3beca6c7bce378dc135b
Steven <stevengf14@gmail.com>
2022-12-28 20:51:27 -0500

    Testing controlled components - form submit event

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -21,4 +21,19 @@ describe("CommentBox", () => {
     const component = renderComponent(CommentBox);
     expect(component.find("button")).to.exist;
   });
+
+  describe("entering some text", () => {
+    beforeEach(() => {
+      component.find("textarea").simulate("change", "new comment");
+    });
+
+    it("shows that text in the textarea", () => {
+      expect(component.find("textarea")).to.have.value("new comment");
+    });
+
+    it("when submitted, clears the input", () => {
+      component.simulate("submit");
+      expect(component.find("textarea")).to.have.value("");
+    });
+  });
 });
