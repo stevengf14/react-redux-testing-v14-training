02d7d5a0a9cf3011ed08d5d8d21fbd19310aee08
Steven <stevengf14@gmail.com>
2023-01-11 21:32:38 -0500

    css style

--- middlewares/src/components/UserList.js
+++ middlewares/src/components/UserList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import "../style/style.css";
 
 function UserList(props) {
   useEffect(() => {
@@ -10,14 +11,16 @@ function UserList(props) {
   const renderUser = (user) => {
     return (
       <div className="card card-block">
-        <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">Cheese Factory</p>
-        <a className="btn btn-primary">Email</a>
+        <div className="card-content">
+          <h4 className="card-title">{user.name}</h4>
+          <p className="card-text">Cheese Factory</p>
+          <a className="btn btn-primary">Email</a>
+        </div>
       </div>
     );
   };
 
-  return <div>{props.users.map(renderUser)}</div>;
+  return <div className="user-list">{props.users.map(renderUser)}</div>;
 }
 
 function mapStateToProps(state) {
