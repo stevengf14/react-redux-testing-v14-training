674162d1aef779f516f9b7fb702c4811a274f8ea
Steven <stevengf14@gmail.com>
2023-01-16 21:15:00 -0500

    LocalStorage and JWT - Auth Error Messaging

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,27 @@
 import axios from "axios";
-import { AUTH_USER } from "./types";
+import { AUTH_USER, AUHT_ERROR } from "./types";
 
 const ROOT_URL = "http://localhost:3090";
 
-export function signinUser({ email, password }) {
-  return function (dispatch) {
+export const signinUser =
+  ({ email, password }, callback) =>
+  async (dispatch) => {
     console.log("action", email, password);
-    // Submit email/password to the server
     axios
       .post(`${ROOT_URL}/signin`, { email, password })
       .then((response) => {
-        // If request is good...
-        // - Update state to indicate user is authenticated
+        localStorage.setItem("token", response.data.token);
         dispatch({ type: AUTH_USER });
-        // - Save the JWT token
-        // - Redirect to the route '/featute'
       })
       .catch(() => {
-        // If request is bad...
-        // - Show an error to the user
+        dispatch(authError("Bad Login Info"));
       });
+    callback();
   };
-}
+
+export const authError = (error) => {
+  return {
+    type: AUHT_ERROR,
+    payload: error,
+  };
+};
