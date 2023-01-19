f1c1305f80a7181ec1b11b96729b3358ea8ae54f
Steven <stevengf14@gmail.com>
2023-01-18 23:21:11 -0500

    Signup form scaffolding

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,8 +1,51 @@
 import React from "react";
-import { reduxForm } from "redux-form";
+import { Field, reduxForm } from "redux-form";
 import * as actions from "../../actions";
 
-function Signup() {
-  return <div>Signup</div>;
+function Signup(props) {
+  const {
+    handleSubmit,
+    fields: { email, password, passwordConfirm },
+  } = props;
+  return (
+    <form>
+      <fieldset className="form-group">
+        <label>Email:</label>
+        <Field
+          className="form-control"
+          name="email"
+          type="text"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <fieldset className="form-group">
+        <label>Password:</label>
+        <Field
+          className="form-control"
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <fieldset className="form-group">
+        <label>Confirm Password:</label>
+        <Field
+          className="form-control"
+          name="passwordConfirm"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <button action="submit" className="btn btn-primary">
+        Sign up!
+      </button>
+    </form>
+  );
 }
-export default Signup;
+export default reduxForm({
+  form: "signup",
+  fields: ["email", "password", "passwordConfirm"],
+})(Signup);
